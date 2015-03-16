4ecc30d4699a1a5758d2cae15494124add6d4ef4
Antonio Murdaca <me@runcom.ninja>
2015-03-16 18:22:45 +0100

    supervisor conf on s3

--- bin/deploy_s3.sh
+++ bin/deploy_s3.sh
@@ -3,6 +3,7 @@
 GIT_VERSION="$(git rev-parse --verify HEAD | cut -b 1-15)"
 make
 aws s3 cp s3://prima-deploy/parameters_go.yml parameters_prod.yml
+aws s3 cp s3://prima-deploy/gobin_supervisord.conf supervisord.conf
 zip -9 -r -q "gobin_$GIT_VERSION.zip" parameters_prod.yml dlx/dlx bin/start.sh supervisord.conf Dockerfile Dockerrun.aws.json
 aws s3 cp "gobin_$GIT_VERSION.zip" s3://prima-deploy/gobin/"gobin_$GIT_VERSION.zip"
 aws elasticbeanstalk create-application-version --application-name prima-go --version-label prima-go-$GIT_VERSION --description "Deploy master" --source-bundle S3Bucket=prima-deploy,S3Key="gobin/gobin_$GIT_VERSION.zip" --no-auto-create-application
